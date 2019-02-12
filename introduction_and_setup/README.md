743d153f50aea575b60a5b674edea0a1db7ad3b3
hoangTien <nguyen.hoang.tien@framgia.com>
2019-02-12 10:39:31 +0700

    #1. Installation bash script

---
+++ introduction_and_setup/README.md
@@ -0,0 +1,14 @@
+# 1. Introduction and Setup
+## Directly install to current machine via binary:
+```
+$ sudo chmod +x ./introduction_and_setup/setup.sh
+$ sudo ./introduction_and_setup/setup.sh binary
+```
+
+## Run up a docker container:
+- **Note**: this options require pre-installed docker
+
+```
+$ sudo chmod +x ./introduction_and_setup/setup.sh
+$ ./introduction_and_setup/setup.sh docker
+```
--- introduction_and_setup/setup.sh
+++ introduction_and_setup/setup.sh
@@ -11,13 +11,48 @@ fi
 
 command_action=${1-}
 
-
 function binary() {
-    echo "Replace this code"
+  # Checking for nvm
+  if ! which nvm >/dev/null 2>&1; then
+    echo "nvm isn't installed. Installing nvm."
+    if ! which curl >/dev/null 2>&1; then
+      echo "curl isn't installed. Installing curl."
+      apt-get -y install curl
+    fi
+
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+  fi
+
+  echo "Nvm version: $(nvm --version)"
+  nvm install node
+
+  echo "Node version: $(node -v)"
+  echo "Npm version: $(npm -v)"
+
+  echo "Close and reopen your terminal to start or run the following to use it now:
+export NVM_DIR=\"$HOME/.nvm\"
+  [ -s \"$NVM_DIR/nvm.sh\" ] && \. \"$NVM_DIR/nvm.sh\"
+  [ -s \"$NVM_DIR/bash_completion\" ] && \. \"$NVM_DIR/bash_completion\"
+  "
 }
 
-function docker() {
-    echo "Replace this code"
+function dockerEnv() {
+  if ! which docker >/dev/null 2>&1; then
+    echo "Docker isn't installed. Please install Docker before continue."
+    exit
+  fi
+
+  containerName="node-env"
+  if [ ! "$(docker ps -a -q --filter name=$containerName)" ]; then
+    echo "Container isn't exist. Creating"
+    docker run -v $(pwd):/var/www/app -w /var/www/app -p 8080:8080 --name $containerName -it node:11 /bin/bash
+  else
+    echo "Container is already exist. Starting"
+    docker start $containerName -i
+  fi
 }
 
 case "$command_action" in
@@ -26,7 +61,7 @@ case "$command_action" in
     echo "✹✹✹ Node.js has been installed with Node Version Manager ✹✹✹"
     ;;
   "docker")
-    docker
+    dockerEnv
     echo "✹✹✹ Node.js container has been up and running ✹✹✹"
     ;;
   *)
